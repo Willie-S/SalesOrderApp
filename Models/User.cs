71fa35743c5afc91ae2c829d6b4d92224212b22b
SwanepoelWB <wbswanepoel@outlook.com>
2024-07-31 12:16:56 +0200

    -Updated properties of XML data files such that they're included in the build and copied to the output directory; -Updated JWT key-value pairs; -Updated User entity to inherit from Base entity;

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace SalesOrderApp.Models
 {
-    public class User
+    public class User : BaseEntity
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
